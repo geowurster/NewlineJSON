5bad08ee0929b3bc9c8e554fda3f3cdbbc83c8aa
kevin Wurster <wursterk@gmail.com>
2015-01-05 23:17:02 -0500

    Test multiple JSON libraries

--- tests/test_core.py
+++ tests/test_core.py
@@ -23,6 +23,9 @@ import yajl
 import newlinejson
 
 
+JSON_LIBRARIES = [json, simplejson, ujson, yajl]
+
+
 LIST_WITH_HEADER = """["field1","field2","field3"]
 ["l1f1","l1f2","l1f3"]
 ["l2f1","l2f2","l3f3"]
@@ -44,7 +47,7 @@ class TestReader(unittest.TestCase):
     def test_list_with_header(self):
 
         # Read a file that contains only lists and has a header row
-        for json_library in [json, simplejson, ujson, yajl]:
+        for json_library in JSON_LIBRARIES:
             newlinejson.JSON = json_library
             for expected, actual in zip(StringIO(LIST_WITH_HEADER), newlinejson.Reader(StringIO(LIST_WITH_HEADER))):
                 self.assertEqual(json.loads(expected), actual)
@@ -52,7 +55,7 @@ class TestReader(unittest.TestCase):
     def test_list_no_header(self):
 
         # Read a file that contains only lists and does not have a header row
-        for json_library in [json, simplejson, ujson, yajl]:
+        for json_library in JSON_LIBRARIES:
             newlinejson.JSON = json_library
             for expected, actual in zip(StringIO(LIST_NO_HEADER), newlinejson.Reader(StringIO(LIST_NO_HEADER))):
                 self.assertEqual(json.loads(expected), actual)
@@ -60,7 +63,7 @@ class TestReader(unittest.TestCase):
     def test_dict_lines(self):
 
         # Read a file that contains only dictionaries on every line
-        for json_library in [json, simplejson, ujson, yajl]:
+        for json_library in JSON_LIBRARIES:
             newlinejson.JSON = json_library
             for expected, actual in zip(StringIO(DICT_LINES), newlinejson.Reader(StringIO(DICT_LINES))):
                 self.assertEqual(json.loads(expected), actual)
