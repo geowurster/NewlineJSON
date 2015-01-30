09b2984eebe4a3345bba065b13883bced6be3826
kevin Wurster <wursterk@gmail.com>
2015-01-29 23:24:37 -0500

    Centralized some common testing objects

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,5 +2,29 @@
 Unittests for newlinejson
 """
 
+import json
+try:
+    from io import StringIO
+except ImportError:
+    try:
+        from cStringIO import StringIO
+    except ImportError:
+        from StringIO import StringIO
+
+try:
+    import simplejson
+except ImportError:
+    simplejson = None
+try:
+    import ujson
+except ImportError:
+    ujson = None
+try:
+    import yajl
+except ImportError:
+    yajl = None
 
 from . import *
+
+
+JSON_LIBRARIES = [lib for lib in (json, ujson, simplejson, yajl) if lib is not None]
